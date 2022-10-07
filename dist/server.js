f3f900ec84ef405d1e1738f70df1ce06fa7d3093
fabian <fabiandavid2403@gmail.com>
2022-10-06 21:58:08 -0500

    remove npm dependencies innessaries

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./models/app.js"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_app.default.listen(8080, () => {
-  console.log("server on port 8080");
-});
