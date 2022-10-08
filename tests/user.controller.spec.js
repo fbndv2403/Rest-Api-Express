5cdc59de84805404563b98413b06326cc24c721f
fabian <fabiandavid2403@gmail.com>
2022-10-08 01:55:36 -0500

    dev: basic crud routes

--- tests/user.controller.spec.js
+++ tests/user.controller.spec.js
@@ -14,8 +14,26 @@ describe("Main Endpoints", () => {
 describe("Post Endpoint", () => {
   it("POST /api/post route: post 201 created", async () => {
     await request(app)
-    .post("/api/post")
-    .expect(201)
-    .expect("Content-Type", /json/);
-  })
-})
+      .post("/api/post")
+      .expect(201)
+      .expect("Content-Type", /json/);
+  });
+});
+
+describe("Put Endpoint", () => {
+  it("PUT /api/put route: put 201 created", async () => {
+    await request(app)
+      .put("/api/put")
+      .expect(201)
+      .expect("Content-Type", /json/);
+  });
+});
+
+describe("Delete Endpoint", () => {
+  it("DELETE /api/delete route: delete 202 acepted", async () => {
+    await request(app)
+      .delete("/api/delete")
+      .expect(202)
+      .expect("Content-Type", /json/);
+  });
+});
